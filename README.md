85959dad46ff07563db5106dde5c66ddb2caa815
gaidap <pc.gaida@gmail.com>
2021-04-02 20:44:32 +0200

    commit to test travis ci build and deployment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Overly complex Fibonacci Calculator
 This a learning project with an on purpose over-engineered approach on an fibonacci calculator, to learn
-and demonstrate how to deploy a very complex app with multiple components in docker.
+and demonstrate how to deploy a very complex app with multiple components in kubernetes.
 
 ## My disclaimer
-Be warned this is only a learning project. All code residing in this project and or repo is my attempt to learn docker. So it is possible that some code maybe not best practice or standard. 
+Be warned this is only a learning project. All code residing in this project and or repo is my attempt to learn kubernetes. So it is possible that some code maybe not best practice or standard. 
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t pgaida/fib-client:latest -t pgaida/fib-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t pgaida/fib-server:latest -t pgaida/fib-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t pgaida/fib-worker:latest -t pgaida/fib-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push pgaida/fib-client:latest
+docker push pgaida/fib-client:$GIT_SHA
+docker push pgaida/fib-server:latest
+docker push pgaida/fib-server:$GIT_SHA
+docker push pgaida/fib-worker:latest
+docker push pgaida/fib-worker:$GIT_SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=pgaida/fib-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=pgaida/fib-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=pgaida/fib-worker$GIT_SHA
+
